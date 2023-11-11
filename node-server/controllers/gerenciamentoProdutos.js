1fb6e64cbf904e1cfa54edc276710babb62c80f4
Vitor Leitao <vml2@cin.ufpe.br>
2023-11-11 18:08:19 -0300

    feat: adicionando funcionalidades na feature de cadastro de produtos

--- node-server/controllers/gerenciamentoProdutos.js
+++ node-server/controllers/gerenciamentoProdutos.js
@@ -1,20 +1,58 @@
 // Importando todos os tipos de criaçaõ de entidades
 const entidades = require('../models/entidades');
 
+
+// Cadastro de produtos
+// Adiconar a criação do estoque para esse produto
 exports.cadastroProdutos = async (req, res) => {
     //res.send(req.body);
     try {
-        // Cria uma nova instância de entidades
+        // Cadastrando o produto no banco de dados 
         const novoProduto = await entidades.productCreate.create({
             nome: req.body.nome,
             categoria: req.body.categoria,
+            descricao: req.body.descricao,
             preco: req.body.preco,
-            quantidadeEstoque: req.body.quantidadeEstoque
         });
 
-        res.json(novoProduto);
+        // Cadastrando esse produto no estoque
+        const novoEstoque = await entidades.ofereceCreate.create({
+            productID: novoProduto.productID,
+            cnpjLoja: req.body.cnpjLoja,
+            quantidadeEstoque: req.body.quantidadeEstoque,
+        });
+
+        res.json({
+            novoProduto: novoProduto,
+            novoEstoque: novoEstoque
+        });
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error interno do servidor');
+        res.status(500).send('Erro interno do servidor');
     }
 }
+
+// Consultar todos os produtos de uma loja
+exports.consultarProdutosCnpj = async (req, res) => {
+    try{
+        const cnpjLoja = req.paramns.cnpj;
+        
+
+    }catch(error){
+        res.status(500).send('Erro interno do servidor');
+    }
+}
+
+
+/*
+Usar isso no delete producs:
+// Desativa as verificações de chave estrangeira
+await db.query('SET FOREIGN_KEY_CHECKS = 0', { raw: true });
+
+// Trunca a tabela
+await Product.destroy({ truncate: true });
+
+// Ativa as verificações de chave estrangeira novamente
+await db.query('SET FOREIGN_KEY_CHECKS = 1', { raw: true });
+
+*/
