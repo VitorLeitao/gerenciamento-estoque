ba7c489dfb5cc544239c58e07e0fae64e535cce1
Vitor Leitao <vml2@cin.ufpe.br>
2023-11-11 18:11:20 -0300

    feat: atualizando a chamada das rotas

--- node-server/routes.js
+++ node-server/routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const route = express.Router();
 const gerenciamentoProdutos = require('./controllers/gerenciamentoProdutos')
+const gerenciamentoLojas = require('./controllers/gerenciamentoLojas')
 const paginaInicial = require('./controllers/paginaInicial')
 
 
 route.get('/', paginaInicial.olaMundo);
-route.post('/cadastrar', gerenciamentoProdutos.cadastroProdutos);
+// Rotas de gerenciamento de produtos
+route.post('/cadastrarProduto', gerenciamentoProdutos.cadastroProdutos);
+
+// Rotas de gerenciamento de lojas
+route.post('/cadastrarLoja', gerenciamentoLojas.cadastroLoja);
 
 
 module.exports = route;
