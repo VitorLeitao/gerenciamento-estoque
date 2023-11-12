1a6d4dc5b3240f702d27f4fe245372931a62fbca
Vitor Leitao <vml2@cin.ufpe.br>
2023-11-12 14:43:07 -0300

    feat: adicionando instancias das rotas

--- node-server/routes.js
+++ node-server/routes.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const route = express.Router();
 const gerenciamentoProdutos = require('./controllers/gerenciamentoProdutos')
+const gerenciamentoEstoque = require('./controllers/gerenciamentoEstoque')
 const gerenciamentoLojas = require('./controllers/gerenciamentoLojas')
 const paginaInicial = require('./controllers/paginaInicial')
 
 
 route.get('/', paginaInicial.olaMundo);
 // Rotas de gerenciamento de produtos
-route.post('/cadastrarProduto', gerenciamentoProdutos.cadastroProdutos);
+route.post('/criarProdutoGeral', gerenciamentoProdutos.criarProdutoGeral)
+
+// Rotas para gerenciamento de estoque
+route.post('/cadastroProdutosEstoque', gerenciamentoEstoque.cadastroProdutosEstoque);
+route.get('/produtosByCNPJ/:cnpj', gerenciamentoEstoque.consultarProdutosCnpj);
+route.delete('/cancelarOfertaProduto', gerenciamentoEstoque.cancelarOfertaProduto);
+route.put('/efetuarVenda', gerenciamentoEstoque.efetuarVenda);
+route.put('/efetuarFornecimento', gerenciamentoEstoque.efetuarFornecimento);
 
 // Rotas de gerenciamento de lojas
 route.post('/cadastrarLoja', gerenciamentoLojas.cadastroLoja);
