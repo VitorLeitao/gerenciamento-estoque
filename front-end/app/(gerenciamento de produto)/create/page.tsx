b5d49efad9a3eb8c88a147f4101db2cdfa045e1c
Vitor Leitao <vml2@cin.ufpe.br>
2023-11-15 12:37:22 -0300

    feat: Finalizando pagina de cadastro de produto unico

--- front-end/app/(gerenciamento de produto)/create/page.tsx
+++ front-end/app/(gerenciamento de produto)/create/page.tsx
@@ -2,6 +2,7 @@
 import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { useToast, toast } from "@/components/ui/use-toast"
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
@@ -23,12 +24,14 @@ const FormSchema = z.object({
         .min(1, 'Descrição muito curta')
         .min(8, 'Descrição muito grande'),
     categoria: z.string().min(1, 'Precisa ter uma categoria'),
+    url: z.string().min(1, 'Precisa ter uma categoria'),
 });
 
 interface produtoCreate {
     nome: string,
     descricao: string,
-    categoria: string
+    categoria: string,
+    url: string
 }
 
 const cadastrarProdutoUnico = (novoProduto: produtoCreate) => {
@@ -38,62 +41,113 @@ const cadastrarProdutoUnico = (novoProduto: produtoCreate) => {
         defaultValues: {
             nome: '',
             descricao: '',
-            categoria: ''
+            categoria: '',
+            url: '',
         },
     });
 
-    const handleClick = () => {
-        alert('Clicou no botao!');
+
+    const enviaToast = (title: string, description: string, backgroundColor: string, color: string) => {
+        console.log('Toast enviado:', title, description, backgroundColor, color);
+        toast({
+            title: title,
+            description: description,
+            style: {
+                backgroundColor: backgroundColor,
+                color: color
+            }
+        });
     }
 
-    const handleSumit = (e: React.FormEvent) => {
+    const handleSumit = async (e: React.FormEvent) => {
         e.preventDefault();
-        alert("Formulario recibio");
+
+        // Obtendo os valores do formulário usando o objeto form
+        const { nome, descricao, categoria, url } = await form.getValues();
+        const instanciaProduto: produtoCreate = {
+            nome: nome,
+            descricao: descricao,
+            categoria: categoria,
+            url: url,
+        };
+        console.log(JSON.stringify(instanciaProduto));
+
+        // Vamos usar essa instancia para chamar nossa rota
+        try {
+            const response = await fetch('http://localhost:2800/criarProdutoGeral', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(instanciaProduto),
+            });
+            console.log(response);
+            if (response.status === 200) {
+                enviaToast("Produto adicionado com sucesso", "name", "#4CAF50", "white");
+                form.reset();
+            } else {
+                console.log("Erro ao adicionar produto")
+                enviaToast("Erro ao adicionar produto", "name", "#FF0000", "white");
+            }
+        } catch (error) {
+            enviaToast("Erro ao adicionar produto", "name", "#FF0000", "white");
+        }
     }
+
     return (
         <div>
-            <h1>Criação de produto</h1>
-            <div>
-                <Form {...form}>
-                    <form onSubmit={handleSumit} className="space-y-8">
-                        <FormField
-                            control={form.control}
-                            name="nome"
-                            render={() => (
-                                <FormItem>
-                                    <FormLabel>Nome</FormLabel>
-                                    <FormControl>
-                                        <Input />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+            <div className="flex items-center justify-center h-screen border">
+                <div className="border p-10 shadow-xl rounded-lg bg-white">
+                    <Form {...form}>
+                        <form onSubmit={handleSumit} className="space-y-8">
 
-                        <FormField
-                            control={form.control}
-                            name="descricao"
-                            render={() => (
-                                <FormItem>
-                                    <FormLabel>Descricao</FormLabel>
-                                    <Input />
-                                </FormItem>
-                            )}
-                        />
+                            <FormField 
+                                control={form.control}
+                                name="nome"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className='flex items-center justify-center'>Nome</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
 
-                        <FormField
-                            control={form.control}
-                            name="categoria"
-                            render={() => (
-                                <FormItem>
-                                    <FormLabel>Categoria</FormLabel>
-                                    <Input />
-                                </FormItem>
-                            )}
-                        />
-                        <Button type="submit">Submit</Button>
-                    </form>
-                </Form>
+                            <FormField
+                                control={form.control}
+                                name="descricao"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className='flex items-center justify-center'>Descricao</FormLabel>
+                                        <Input {...field} />
+                                    </FormItem>
+                                )}
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="categoria"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className='flex items-center justify-center'>Categoria</FormLabel>
+                                        <Input {...field} />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="url"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className='flex items-center justify-center'>url</FormLabel>
+                                        <Input {...field} />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button className="w-full" type="submit">Submit</Button>
+                        </form>
+                    </Form>
+                </div>
             </div>
         </div>
     );
