c4a076ea1807ad4551ea50b9f89ecf089c024d95
Vitor Leitao <vml2@cin.ufpe.br>
2023-11-15 12:33:55 -0300

    feat: Adicionando configuracoes de CORS

--- node-server/app.js
+++ node-server/app.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = express.Router();
+const cors = require('cors');  // Adicione esta linha para importar o módulo 'cors'
 const routes = require('./routes');
 
 
 const app = express();
+
+// Configuração do CORS: Aceitar requisições do front end
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 
 // Fazendo o server utilizar as rotas
 app.use(routes);
 
-app.listen(3000, ()=>{
+app.listen(2800, ()=>{
     console.log('Servidor iniciado!');
 })
